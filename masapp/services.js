9e09c6d9e899b63e552571bb8df362d356a3e0da
Affapple <affectingapple1@gmail.com>
2023-01-05 16:53:21 +0000

    commit great

--- masapp/services.js
+++
@@ -1,7 +0,0 @@
-function organizarPedido(){
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const aeroporto = urlParams.get('idAirport');
-    const hotel = urlParams.get('idHotel');
-
-}
